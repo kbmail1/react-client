960b90004374807cb8ce1418baff90912d038b32
kundan Bapat <kundan.bapat@gmail.com>
2022-01-10 22:21:07 -0500

    x-word - refactor

--- src/PWA.tsx
+++ src/PWA.tsx
@@ -19,14 +19,14 @@ const PWA = () => {
   const arr = Array(grid.rows * grid.cols).fill(<div>&nbsp;</div>)
 
   const rowcol2Id = (row: number, col: number): number => {
-    const id = (grid.rows * row) + col
+    const id = (grid.cols * col) + row
     console.log(id)
     return id
   }
 
   const id2Rowcol = ((id: number): [number, number] => {
-    const row = Math.floor(id / grid.rows)
-    const col = id % grid.cols
+    const row = id % grid.rows
+    const col = Math.floor(id / grid.cols)
     return [row, col]
   })
 
@@ -35,43 +35,25 @@ const PWA = () => {
     const id = Number(rowcol2Id(row, col))
   })
 
-  const placeWordByRowcol = ((row: number, col: number, word: string, dimension: string) => {
-    if (dimension !== 'x' && dimension !== 'y') {
-      dimension = 'x'
-    }
-    const startId = rowcol2Id(row, col)
-    console.log(`placeWordByRowcol: startId: ${startId}`)
+  const placeWord = ((word: string, row: number, col: number, axis: string) => {
+    let [x, y] = [row, col]
 
-    // ASSUME word fits
+    for (let wordInd = 0; wordInd < word.length; wordInd++) {
+      const id = rowcol2Id(x, y)
 
-    // for 'x' - increment ids by 1.
-    if (dimension === 'x') {
-      for (let letterIndex = 0, id = startId; id < startId + word.length; id++, letterIndex++) {
-        console.log(`placeWordByRowcol: in for(): id${id}`)
-        const elem = document.getElementById('' + id)
-        if (elem) {
-          console.log(`placeWordByRowcol: in if(): placing letter ${word[id - startId]} at id: ${id}`)
-          elem.innerHTML = '<span class="green">' + word[letterIndex] + '</span>'
-          elem.classList.add('red')
-        } else {
-          console.log(`placeWordByRowcol: else (): no place with id: ${id}`)
-        }
-      }
-    } else {
-      // for 'y' - increment by 'grids.col that is 10.'
-      const inc = grid.rows; // TODO: This works only for SQUARE GRIDS.
-      for (let letterIndex = 0, id = startId; id < startId + (word.length * inc); id += inc, letterIndex++) {
-        console.log(`placeWordByRowcol: in for(): id: ${id}`)
-        const elem = document.getElementById('' + id)
-        if (elem) {
-          console.log(`placeWordByRowcol: in if(): placing letter ${word[id - startId]} at id: ${id}`)
-          elem.innerHTML = '<span class="navy">' + word[letterIndex] + '</span>'
-        } else {
-          console.log(`placeWordByRowcol: else (): no place with id: ${id}`)
-        }
+      const elem = document.getElementById('' + id)
+      if (elem) {
+        elem.innerHTML = '<span class="green">' + word[wordInd] + '</span>'
+      } else {
+        console.log(`Error: placeWordByRowcol: else (): no place with id: ${id}`)
+        return
       }
 
-
+      if (axis === 'x') {
+        x++
+      } else {
+        y++
+      }
     }
   })
 
@@ -80,8 +62,8 @@ const PWA = () => {
     const id = Number(e.target.id)
     console.log('handleClick: id: ', id)
     const [row, col] = id2Rowcol(id)
-    console.log(`row/col: ${row}, ${col}}`)
-    placeWordByRowcol(row, col, 'Hello', 'x')
+    console.log(`handleClick: row/col: ${row}/${col}}`)
+    placeWord('Hello', row, col, 'x')
   })
 
   return (
