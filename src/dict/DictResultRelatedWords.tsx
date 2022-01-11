4f34de02ff7c475c69a195d7e8a448f60920040c
kundan Bapat <kundan.bapat@gmail.com>
2022-01-11 01:24:47 -0500

    dict - refactor - syn and anto into their own file. step1

--- src/dict/DictResultRelatedWords.tsx
+++ src/dict/DictResultRelatedWords.tsx
@@ -5,31 +5,40 @@ import './DictResult.scss'
 
 const DictResultRelatedWords = (props) => {
 
-  // if one synonym has two words, each must be separately
-  // .. linked and searchable in dict, in turn.
   return (
-    <li key={getUniqueKey()}>
-      {
-        props.phrase.split(' ').map((oneWordInS) => {
-          return (
-          <>
-            <span>
-              <button
-                style={{ background: 'none', border: '0', borderBottom: '1px solid lightgray', color: '#367588', cursor: 'pointer' }}
-                onClick={() => props.callback(oneWordInS)}
-              >
-                {oneWordInS}
-              </button>
-            </span>
-            &nbsp;
-          </>
-          )
-        })
-      }
-    </li>
-
-
+    <div className="dict-table-cell">
+      <ul style={{ listStyle: 'none' }}>
+        {
+          props.relatedWords.map((phrase) => {
+            console.log('phrase is: ', phrase);
+            console.log('phrase when split is: ', phrase.split(' '));
+            // if one synonym has two words, each must be separately
+            // .. linked and searchable in dict, in turn.
+            return (
+              <li key={getUniqueKey()}>
+                {
+                  props.phrase.split(' ').map((oneWordInS) => {
+                    return (
+                      <>
+                        <span>
+                          <button
+                            style={{ background: 'none', border: '0', borderBottom: '1px solid lightgray', color: '#367588', cursor: 'pointer' }}
+                            onClick={() => props.callback(oneWordInS)}
+                          >
+                            {oneWordInS}
+                          </button>
+                        </span>
+                        &nbsp;
+                      </>
+                    )
+                  })
+                }
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
   )
-}
 
-export default DictResultRelatedWords
+  export default DictResultRelatedWords
